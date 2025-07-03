ef172299b3c4d28dddbbca8d45307dc06de67ed8
Igor <igorek.kopilov@yandex.ru>
2025-07-03 21:05:03 +0300

    Исправить code style 2.3

--- main/src/main/java/ru/practicum/main/MainApp.java
+++ main/src/main/java/ru/practicum/main/MainApp.java
@@ -3,7 +3,7 @@ package ru.practicum.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "ru.practicum")
+@SpringBootApplication(scanBasePackages = {"ru.practicum.main", "ru.practicum.statistics.client"})
 public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
