0233a128c10c86635d0aaf266a990d7b9a4fd6ff
Igor <igorek.kopilov@yandex.ru>
2025-07-06 16:46:07 +0300

    Исправить работу метода getEventById()

--- main/src/main/java/ru/practicum/main/events/public_api/EventPublicService.java
+++ main/src/main/java/ru/practicum/main/events/public_api/EventPublicService.java
@@ -98,6 +98,12 @@ public class EventPublicService {
         }
         statsClient.createStats("EventPublicService", httpServletRequest.getRequestURI(),
                 httpServletRequest.getRemoteAddr());
+
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         return statistics.searchStatistics(List.of(eventDto)).getFirst();
     }
 }
