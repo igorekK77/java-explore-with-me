8b95093c5c659d9657cf402568d164baf5f86959
Igor <igorek.kopilov@yandex.ru>
2025-07-07 22:50:45 +0300

    Исправить EventPrivateController 2

--- main/src/main/java/ru/practicum/main/events/private_api/EventPrivateService.java
+++ main/src/main/java/ru/practicum/main/events/private_api/EventPrivateService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import ru.practicum.main.categories.Category;
 import ru.practicum.main.categories.CategoryStorage;
 import ru.practicum.main.events.*;
 import ru.practicum.main.events.dto.*;
