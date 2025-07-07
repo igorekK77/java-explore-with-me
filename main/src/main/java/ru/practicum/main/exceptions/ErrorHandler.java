16c42e0b03d233ecd6f374e76b7475628396282d
Igor <igorek.kopilov@yandex.ru>
2025-07-07 21:58:57 +0300

    Исправить обработку ошибок

--- main/src/main/java/ru/practicum/main/exceptions/ErrorHandler.java
+++ main/src/main/java/ru/practicum/main/exceptions/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.main.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -44,6 +45,12 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handlerForbiddenException(final ForbiddenException e) {
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(MethodArgumentNotValidException ex) {
+        return new ErrorResponse(ex.getBindingResult().getAllErrors().getFirst().getDefaultMessage());
     }
 
 }
